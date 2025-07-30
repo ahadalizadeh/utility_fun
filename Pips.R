6bba0a349cdff883559b63fb417c42b9866ca6e2
Ahad Alizadeh <st.alizadeh@gmail.com>
2025-07-30 15:26:16 +0330

    Update Pips.R

--- Pips.R
+++ Pips.R
@@ -67,5 +67,57 @@
   eval( temp, envir = globalenv())
   rm(m.temp.12544522, envir = globalenv())}
 }
+#########################################################
+#####################easy to do
+
+print.DS = function(res){
+  d = paste0(dim(res$data), collapse = ", ")
+  length.vars      = length(res$vars)
+  length.responses = length(res$responses)
+  length.results = length(res$results)
+  if(length.vars==0) {vars = "NULL"} else{ vars = paste0(res$vars, collapse = ", ")}
+  if(length.responses==0) {responses = "NULL"} else {responses = paste0(res$responses, collapse = ", ")}
+  cat(glue::glue("Object of DS class:\n data with {d}  dimentions.\n vars vector: {vars}.\n response vector: {responses}.\n length of results: {length.results}"))
+}
+
+responses = function(res, ...,add=FALSE){
+  responses <- unique(c(...))
+  if("data.frame" %in% class(res) & !("DS" %in% class(res) )){
+    if(any(!responses %in% names(res))) 
+      stop(
+        paste0("the response(s) is not exist:  ", 
+               paste0(responses[which(!responses %in% names(res))], collapse  = ", ")))
+    
+    res = list(data = res, vars= NULL, responses = responses, results = NULL)
+    class(res)<-     "DS"
+  }
+  if("DS" %in% class(res)){
+    if(add) res$responses = unique(c(res$responses , responses)) else {
+      res$responses = responses  
+    }
+  }
+  class(res) = c("DS")
+  res
+}
 
 
+
+groups = function(res, ..., add = FALSE){
+  vars <- unique(c(...))
+  if("data.frame" %in% class(res) & !("DS" %in% class(res))){
+    if(any(!vars %in% names(res))) 
+      stop(
+        paste0("the var(s) is not exist:  ", 
+               paste0(vars[which(!vars %in% names(res))], collapse  = ", ")))
+    
+    res = list(data = res, vars= vars, responses = NULL, results = NULL )
+    class(res)<-     "DS"
+  }
+  if("DS" %in% class(res)){
+    if(add) res$vars = unique(c(res$vars , vars)) else {
+     res$vars = vars   
+    }
+  }
+  class(res) = c("DS")
+  res
+}
